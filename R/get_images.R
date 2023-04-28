9fdf3f6d2d2e2902b4efe2b6339461fab9413644
Paul Johnson <paul.johnson50@nhs.net>
2023-04-28 17:43:52 +0100

    Clean up functions

--- R/get_images.R
+++ R/get_images.R
@@ -1,29 +1,34 @@
 #' get_images
 #'
-#' View images and icons saved within the package and the relevant file paths to utilise them.
+#' View images and icons saved within the package and the relevant file paths
+#' to utilise them.
 #'
-#' @return
 #' @export
-#'
-#' @examples
-get_images <- function(){
+get_images <- function() {
+  # read all files in package
+  temp <- list.files(
+    path = system.file("images", package = "scwplot"),
+    full.names = TRUE
+    )
 
-  temp = list.files(path = system.file("Brand_Images", package="scwplot"), full.names = TRUE) ##read all files in package
+  lapply(temp, fp <- function(x) {
+    system.file("images", paste0(x), package = "scwplot")
+  })
 
-  lapply(temp, fp <- function(x){ system.file("Brand_Images", paste0(x), package="scwplot") })
-
-  df <- dplyr::tibble(index = c(1:length(temp)), Image = temp, Image_File_Path = paste0("\"", temp, "\"" ))
+  df <- dplyr::tibble(
+    index = c(1:length(temp)),
+    Image = temp,
+    Image_File_Path = paste0("\"", temp, "\"")
+    )
 
   img_table <- df |>
     gt::gt() |>
     gt::text_transform(
-      locations = gt::cells_body(c(Image)),
+      locations = gt::cells_body(c('Image')),
       fn = function(Image) {
-        #  The key is here.
         lapply(Image, gt::local_image)
       }
     )
 
   print(img_table)
-
 }
--- R/save_images.R
+++ R/save_images.R
@@ -1,20 +1,18 @@
 #' save_images
 #'
-#' Save images from the get_images() function into your current working directory based on the tabular index.
+#' Save images from the get_images() function into your current working
+#' directory based on the tabular index.
 #'
 #' @param x Vector relating to index column in the output of get_images(). For
-#' example if x equals c(1,2,4) then this will
-#' save images in the output of get_images() with index 1,2 and 4 to the current working directory.
+#' example if x equals c(1,2,4) then this will save images in the output of
+#' get_images() with index 1,2 and 4 to the current working directory.
 #'
-#' @return
 #' @export
-#'
-#' @examples
-save_images<-function(x){
+save_images <- function(x) {
+  temp <- list.files(
+    # read all files in package
+    path = system.file("images", package = "scwplot"), full.names = TRUE
+  )
 
-  temp = list.files(path=system.file("Brand_Images",package="scwplot"),full.names = TRUE) ##read all files in package
-  file.copy(temp[c(x)], to=getwd())
-
-
-
+  file.copy(temp[c(x)], to = getwd())
 }
