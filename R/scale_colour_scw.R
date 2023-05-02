74d3b18e765d566161c8330b881f4c42bc97bbdb
Paul Johnson <paul.johnson50@nhs.net>
2023-05-02 16:35:41 +0100

    Clean up colour palettes and their plot functions

--- R/scale_colour_scw.R
+++ R/scale_colour_scw.R
@@ -1,19 +1,21 @@
 #' Colour scale constructor for SCW theme colours
 #'
 #' @param palette Character name of palette in SCW theme
-#' @param discrete Boolean indicating whether colour aesthetic is discrete or not
+#' @param discrete Boolean indicating whether colour aesthetic is discrete or
+#' not
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments passed to discrete_scale() or
-#'            scale_colour_gradientn(), used respectively when discrete is TRUE or FALSE
+#' scale_colour_gradientn(), used respectively when discrete is TRUE or FALSE
 #'
 #' @importFrom ggplot2 discrete_scale scale_colour_gradientn
 #' @export
 
-scale_colour_scw <- function(palette = "scw", discrete = TRUE, reverse = FALSE, ...) {
+scale_colour_scw <- function(palette = "seq", discrete = TRUE,
+                             reverse = FALSE, ...) {
   pal <- scw_palette(palette = palette, reverse = reverse)
 
   if (discrete) {
-    discrete_scale("colour", paste0("nhs_", palette), palette = pal, ...)
+    discrete_scale("colour", palette = pal, ...)
   } else {
     scale_colour_gradientn(colours = pal(256), ...)
   }
--- R/scale_fill_scw.R
+++ R/scale_fill_scw.R
@@ -1,19 +1,21 @@
 #' Fill scale constructor for SCW colours
 #'
 #' @param palette Character name of palette in SCW theme
-#' @param discrete Boolean indicating whether colour aesthetic is discrete or not
+#' @param discrete Boolean indicating whether colour aesthetic is discrete or
+#' not
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments passed to discrete_scale() or
-#'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
+#' scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
 #'
 #' @importFrom ggplot2 discrete_scale scale_fill_gradientn
 #' @export
 
-scale_fill_scw <- function(palette = "scw", discrete = TRUE, reverse = FALSE, ...) {
+scale_fill_scw <- function(palette = "qual", discrete = TRUE,
+                           reverse = FALSE, ...) {
   pal <- scw_palette(palette = palette, reverse = reverse)
 
   if (discrete) {
-    discrete_scale("fill", paste0("nhs_", palette), palette = pal, ...)
+    discrete_scale("fill", palette, palette = pal, ...)
   } else {
     scale_fill_gradientn(colours = pal(256), ...)
   }
