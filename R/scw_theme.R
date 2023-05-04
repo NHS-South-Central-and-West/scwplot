b121bf2c59bd4184c1a221c800608cddf0f77d6c
Paulj1989 <Paulj1989@users.noreply.github.com>
2023-05-04 09:40:07 +0000

    Style code (GHA)

--- R/scw_theme.R
+++ R/scw_theme.R
@@ -18,7 +18,6 @@
 #'   theme_scw()
 #'
 theme_scw <- function(base_size = 15, base_family = "sans") {
-
   # Use theme_minimal as basis for theme
   ggplot2::theme_minimal(
     base_size = base_size,
@@ -31,53 +30,70 @@ theme_scw <- function(base_size = 15, base_family = "sans") {
       plot.margin = ggplot2::margin(t = 25, b = 25, l = 25, r = 25),
 
       # Text Elements
-      plot.title = ggplot2::element_text(colour = "grey10",
-                                         size = ggplot2::rel(1.6),
-                                         face = "bold",
-                                         hjust = 0),
-      plot.subtitle = ggplot2::element_text(colour = "grey40",
-                                            size = ggplot2::rel(1.2),
-                                            hjust = 0,
-                                            lineheight = 1.4,
-                                            margin = ggplot2::margin(t = 15,
-                                                                     b = 15)),
-      plot.caption = ggplot2::element_text(colour = "grey60",
-                                           size = ggplot2::rel(0.9),
-                                           hjust = 0,
-                                           margin = ggplot2::margin(t = 25)),
+      plot.title = ggplot2::element_text(
+        colour = "grey10",
+        size = ggplot2::rel(1.6),
+        face = "bold",
+        hjust = 0
+      ),
+      plot.subtitle = ggplot2::element_text(
+        colour = "grey40",
+        size = ggplot2::rel(1.2),
+        hjust = 0,
+        lineheight = 1.4,
+        margin = ggplot2::margin(
+          t = 15,
+          b = 15
+        )
+      ),
+      plot.caption = ggplot2::element_text(
+        colour = "grey60",
+        size = ggplot2::rel(0.9),
+        hjust = 0,
+        margin = ggplot2::margin(t = 25)
+      ),
 
       # Legend Elements
       legend.position = "top",
       legend.title = ggplot2::element_blank(),
-      legend.text = ggplot2::element_text(colour = "grey20",
-                                          size = ggplot2::rel(1.2)),
+      legend.text = ggplot2::element_text(
+        colour = "grey20",
+        size = ggplot2::rel(1.2)
+      ),
       legend.key.width = ggplot2::unit(2, "cm"),
       legend.key = ggplot2::element_blank(),
 
       # Axis Elements
-      axis.title.x = ggplot2::element_text(colour = "grey20",
-                                         size = ggplot2::rel(1.2),
-                                         vjust = -2.5),
-      axis.title.y = ggplot2::element_text(colour = "grey20",
-                                           size = ggplot2::rel(1.2),
-                                           vjust = 5,
-                                           angle = 90),
-      axis.text = ggplot2::element_text(colour = "grey20",
-                                        size = ggplot2::rel(1)),
+      axis.title.x = ggplot2::element_text(
+        colour = "grey20",
+        size = ggplot2::rel(1.2),
+        vjust = -2.5
+      ),
+      axis.title.y = ggplot2::element_text(
+        colour = "grey20",
+        size = ggplot2::rel(1.2),
+        vjust = 5,
+        angle = 90
+      ),
+      axis.text = ggplot2::element_text(
+        colour = "grey20",
+        size = ggplot2::rel(1)
+      ),
       axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, b = 10)),
       axis.line = ggplot2::element_line(linewidth = 1),
 
       # Panel Elements
       panel.grid.major.x = ggplot2::element_blank(),
       panel.grid.minor.x = ggplot2::element_blank(),
-      panel.grid.major.y = ggplot2::element_line(linewidth = 0.5,
-                                                 colour = "grey90",
-                                                 linetype = "dashed"),
+      panel.grid.major.y = ggplot2::element_line(
+        linewidth = 0.5,
+        colour = "grey90",
+        linetype = "dashed"
+      ),
       panel.grid.minor.y = ggplot2::element_blank(),
 
       # Facet Elements
       strip.text = ggplot2::element_text(size = ggplot2::rel(1)),
-
       complete = TRUE
     )
 }
