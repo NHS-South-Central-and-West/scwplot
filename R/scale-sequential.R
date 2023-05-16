435242af543a65b773a9056b7e318b8704f5501b
Paul Johnson <paul.johnson50@nhs.net>
2023-05-16 22:21:27 +0100

    Update file naming conventions

--- R/scale-sequential.R
+++ R/scale-sequential.R
@@ -1,26 +1,3 @@
-#' Generate Colour Palette
-#'
-#' Create a palette, from the supplied colour values, either selecting `n`
-#' evenly spaced colours from the colour values, if `discrete = TRUE`, or an
-#' interpolated palette over the full length of the colour values.
-#'
-#' @param values Colour values
-#' @param discrete Is the palette being fitted to discrete or continuous data?
-#' The default, FALSE, interpolates a continuous colour palette from the
-#' supplied colour values, and TRUE selects `n` evenly spaced colours from the
-#' supplied colour values.
-#'
-pal_ramp <- function(values, discrete = FALSE) {
-  force(values)
-  function(n) {
-    if (discrete) {
-      values[seq(from = 1, to = 9, length.out = n)]
-    } else {
-      grDevices::colorRampPalette(values, alpha = TRUE)(n)
-    }
-  }
-}
-
 #' Sequential Colour Palettes
 #'
 #' Return the SCW sequential colour palettes.
