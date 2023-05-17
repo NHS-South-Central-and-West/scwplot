86fadbb52e2349a334e5620ff219c22348a167b1
Paul Johnson <paul.johnson50@nhs.net>
2023-05-17 20:22:33 +0100

    Update colour palettes

--- R/scale-diverging.R
+++ R/scale-diverging.R
@@ -19,30 +19,32 @@
 #'
 #' @examples
 #' scales::show_col(pal_diverging(palette = "blue_green")(9))
-#' scales::show_col(pal_diverging(palette = "blue_yellow_red", alpha = 0.7)(9))
+#' scales::show_col(pal_diverging(palette = "blue_yellow")(9))
+#' scales::show_col(pal_diverging(palette = "blue_red")(9))
+#' scales::show_col(pal_diverging(palette = "blue_yellow", alpha = 0.7)(9))
 #' scales::show_col(pal_diverging(palette = "blue_red", reverse = TRUE)(9))
 pal_diverging <-
-  function(palette = c("blue_green", "blue_yellow_red", "blue_red"),
+  function(palette = c("blue_green", "blue_yellow", "blue_red"),
            alpha = 1, reverse = FALSE) {
-  palette <- match.arg(palette)
+    palette <- match.arg(palette)
 
-  if (alpha > 1L || alpha <= 0L) stop("alpha must be in (0, 1]")
+    if (alpha > 1L || alpha <= 0L) stop("alpha must be in (0, 1]")
 
-  raw_cols <- if (reverse) {
-    rev(scwplot::palettes$"diverging"[[palette]])
-  } else {
-    scwplot::palettes$"diverging"[[palette]]
+    raw_cols <- if (reverse) {
+      rev(scwplot::palettes$"diverging"[[palette]])
+    } else {
+      scwplot::palettes$"diverging"[[palette]]
+    }
+
+    raw_cols_rgb <- grDevices::col2rgb(raw_cols)
+    alpha_cols <- grDevices::rgb(
+      raw_cols_rgb[1L, ], raw_cols_rgb[2L, ], raw_cols_rgb[3L, ],
+      alpha = alpha * 255L, names = names(raw_cols),
+      maxColorValue = 255L
+    )
+
+    pal_ramp(unname(alpha_cols))
   }
-
-  raw_cols_rgb <- grDevices::col2rgb(raw_cols)
-  alpha_cols <- grDevices::rgb(
-    raw_cols_rgb[1L, ], raw_cols_rgb[2L, ], raw_cols_rgb[3L, ],
-    alpha = alpha * 255L, names = names(raw_cols),
-    maxColorValue = 255L
-  )
-
-  pal_ramp(unname(alpha_cols))
-}
 
 #' SCW Diverging Colour Palettes
 #'
@@ -61,7 +63,7 @@ pal_diverging <-
 #'
 scale_colour_diverging <-
   function(..., alpha = 1, reverse = TRUE, discrete = FALSE,
-           palette = c("blue_green", "blue_yellow_red", "blue_red")) {
+           palette = c("blue_green", "blue_yellow", "blue_red")) {
     palette <- match.arg(palette)
 
     if (discrete) {
@@ -88,7 +90,7 @@ scale_color_diverging <- scale_colour_diverging
 #' @rdname scale_diverging
 scale_fill_diverging <-
   function(..., alpha = 1, reverse = TRUE, discrete = FALSE,
-           palette = c("blue_green", "blue_yellow_red", "blue_red")) {
+           palette = c("blue_green", "blue_yellow", "blue_red")) {
     palette <- match.arg(palette)
 
     if (discrete) {
